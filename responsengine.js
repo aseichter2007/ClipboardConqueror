9175ac6a5902c687b57f7782669b33dd4f400132
aseichter2007 <aseichter2007@gmail.com>
2024-05-18 12:44:32 -0500

    updated for easier function calling.

--- responsengine.js
+++ responsengine.js
@@ -1,43 +1,50 @@
 class ResponseEngine {
-    constructor(invoke =  "<|call|>", invokeTerminator = "<|/call|>",  terminators = ["asdfghjkl"], remove= /<\|[^|]*\|>/g){
-        this.Invoke= invoke;
-        this.terminators = terminators;
+    constructor(
+        functionList =  [{start:"<|call|>", end:"<|/endCall|>", func: "call"} ], remove= /<\|[^|]*\|>/g)
+        {
+        this.functionList= functionList;
         this.remove = remove;
     }
-
+    call(text){
+        console.log("Function called text: " + text);
+    }
+    callFunctions(text){
+        this.functionList.forEach( fun => {
+            this.functionCheckExecute( text, fun.start, fun.end, this[ fun.func ] )
+        });
+    }        
+    functionCheckExecute(text, functionCall, terminator, callback){
+        if (text.includes(functionCall)) {
+            const splitText = text.split(functionCall)
+            if(splitText.length >=1){
+                if (splitText[0].includes(terminator)){
+                    const outText = splitText[0].split(terminator);
+                    callback(outText[0])
+                }
+            }
+        }
+    }
+    //Add your funtions as methods in this file. 
     removeChatML(text) {
-
       return text.replace(this.remove, '');
     }   
-    callFuncion(text){
-        //Check if the message contains the invoke token
-        if (text.includes(this.Invoke)) {
-            // Do something
-        } else {
-            // Do something else
-        }
-    }        
-    
-    
     recieveMessageFindTerminatorsAndTrim(text) {
         let totals = []
+        this.callFunctions(text);
         text = this.removeChatML(text);
-        this.terminators.forEach(element => {
-            
-            const limited = text.split(element);
-            totals.push(limited.length);
-        });
-        let low = 100000;
-        let LowPosition = 0
-       for (let index = 0; index < totals.length; index++) {
-           const element = totals[index]> low;
-           if (element < low) {
-                low = element;
-                LowPosition = index;
-            }
-       }
-       let response = text.split(this.terminators[LowPosition])
-       return response[0];
+      
+    //     let low = 100000;
+    //     let LowPosition = 0
+    //    for (let index = 0; index < totals.length; index++) {
+    //        const element = totals[index]> low;
+    //        if (element < low) {
+    //             low = element;
+    //             LowPosition = index;
+    //         }
+    //    }
+    //    let response = text.split(this.terminators[LowPosition])
+    //    return response[0];
+    return text;
     }
   
 }
